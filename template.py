5dbb70f876c651d1b57120217fbcac759d7f3d4a
riz-coder <amanat.pg3700781@cloud.neduet.edu.pk>
2024-04-17 10:35:15 +0500

    Fix file path in template.py

--- template.py
+++ template.py
@@ -20,7 +20,7 @@ list_of_files = [
     "src/utils/__init__.py",
     "src/utils/utils.py",
     "src/logger/logging.py",
-    "src/exceptions/exception",
+    "src/exceptions/exception.py",
     "tests/unit/__init__.py",
     "tests/integration/__init__.py",
     "init_setup.sh",
